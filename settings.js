a2b3741e70d5f9f0e0b0d897195be8551720aa11
rarabay <115088868+rarabay@users.noreply.github.com>
2022-10-05 19:43:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94771916428'] //ur owner number
+global.ownername = "NIMESH RASINTHA" //ur owner name
+global.ytname = "YT: KING N.R.B" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94771916428']
+global.ownertag = '94771916428' //ur tag number
+global.botnumber = '94771916428' //Add your Bot Number For Log
+global.botname = 'KING N.R.B BOT' //ur bot name
+global.ig = "https://www.youtube.com/channel/UCoHIPCPn7wtscHbulHV12Rw" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCoHIPCPn7wtscHbulHV12Rw" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://www.youtube.com/channel/UCoHIPCPn7wtscHbulHV12Rw'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.packname = "KING N.R.B" //ur sticker watermark packname
+global.author = "KING N.R.B" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
